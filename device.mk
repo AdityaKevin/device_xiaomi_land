f7171e274983b00ef0e26c43719a3a080e5801dd
AdityaKevin <adityakevin50@gmail.com>
2019-07-20 08:08:36 +0000

    land: add msm8937 usb hal

--- device.mk
+++ device.mk
@@ -394,10 +394,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-    
+
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_8937
 
 # Vibrator
 PRODUCT_PACKAGES += \
